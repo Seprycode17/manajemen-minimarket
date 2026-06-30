e4029a2103ebf07e7e5a0d9d191145ed3131ed60
esrasiregar2 <esrasiregar2@gmail.com>
2026-06-30 11:33:14 +0700

    membuat isi providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate; // <--- WAJIB TAMBAHKAN INI
+use App\Models\User; // <--- WAJIB TAMBAHKAN INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // DAFTARKAN GERBANG IZIN ADMIN DI SINI
+        Gate::define('admin', function (User $user) {
+            return $user->role === 'admin';
+        });
     }
 }
